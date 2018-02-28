2b3707dca4a631e0d6c1d27eed3af422fd02eed9
Scott Dobbins <scott@dobbinsweb.com>
2018-02-28 13:50:47 -0600

    asdfasdfasdf

---
+++ Trawick.Rest/Controllers/MemberController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Trawick.Data.Models;
+namespace Trawick.Rest.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult GetMemberInfo(int userID, string authCode)
+        {
+
+            return Json(MemberRepo.Member_GetByUserId(userID), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
