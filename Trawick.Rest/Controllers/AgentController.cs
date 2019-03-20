b35ce104fe631c041d704dbbe637eefaaa619538
sdobbins76 <scott@dobbinsweb.com>
2019-03-20 16:18:29 -0500

    asdfasdfadsf

---
+++ Trawick.Rest/Controllers/AgentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace Trawick.Rest.Controllers
+{
+    public class AgentController : Controller
+    {
+
+        public ActionResult Agent_GetAgentInfo(int AgentId)
+        {
+            var model = Trawick.Data.Models.ContactRepo.Agent_GetAgentInfo(AgentId);
+            if (model != null)
+            {
+                var jS = Newtonsoft.Json.JsonConvert.SerializeObject(model);
+                return Json(jS, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Agent_GetAgentInfoJson(int AgentId)
+        {
+            var model = Trawick.Data.Models.ContactRepo.Agent_GetAgentInfo(AgentId);
+            if (model != null)
+            {
+             
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Trawick.Rest/Controllers/Renewal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace Trawick.Rest.Controllers
+{
+    public class RenewalController : Controller
+    {
+
+        public ActionResult DateInfo(int Id)
+        {
+            var model = Trawick.Data.Models.EnrollmentRepo.Renewal_GetDateInfo(Id);
+            if (model != null)
+            {
+
+                return Json(model, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
